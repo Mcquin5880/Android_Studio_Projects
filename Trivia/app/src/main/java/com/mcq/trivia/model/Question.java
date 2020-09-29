27d5039d939222612002bcc25f31652ec12a7334
NewbieMcCoder <Mquinter1988@gmail.com>
2020-09-28 22:04:49 -0400

    Question bank setup

---
+++ Trivia/app/src/main/java/com/mcq/trivia/model/Question.java
@@ -0,0 +1,31 @@
+package com.mcq.trivia.model;
+
+public class Question {
+
+    private String answer;
+    private boolean answerTrue;
+
+    public Question() { }
+
+    public Question(String answer, boolean answerTrue) {
+        this.answer = answer;
+        this.answerTrue = answerTrue;
+    }
+
+    public String getAnswer() {
+        return this.answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean isAnswerTrue() {
+        return this.answerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        this.answerTrue = answerTrue;
+    }
+
+}
