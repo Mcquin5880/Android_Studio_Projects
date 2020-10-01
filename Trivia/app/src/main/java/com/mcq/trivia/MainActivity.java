89e01f224c5e0e764d0c0c73108cf1beaf5ba314
NewbieMcCoder <Mquinter1988@gmail.com>
2020-09-30 22:53:47 -0400

    Wrapping up section

--- Trivia/app/src/main/java/com/mcq/trivia/MainActivity.java
+++ Trivia/app/src/main/java/com/mcq/trivia/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mcq.trivia.data.AnswerListAsyncResponse;
 import com.mcq.trivia.data.QuestionBank;
@@ -12,27 +17,86 @@ import com.mcq.trivia.model.Question;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private TextView questionTextview;
+    private TextView questionCounterTextview;
+    private Button trueButton;
+    private Button falseButton;
+    private ImageButton nextButton;
+    private ImageButton prevButton;
 
+    private int currentQuestionIndex = 0;
+    private List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Question> questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
+        questionTextview = findViewById(R.id.question_textview);
+        questionCounterTextview = findViewById(R.id.counter_text);
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        nextButton = findViewById(R.id.next_button);
+        prevButton = findViewById(R.id.prev_button);
+
+        nextButton.setOnClickListener(this);
+        prevButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+        falseButton.setOnClickListener(this);
+
+        questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
             @Override
             public void processFinished(ArrayList<Question> questionArrayList) {
 
+                questionTextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+                questionCounterTextview.setText(currentQuestionIndex + " / " + questionArrayList.size());
                 Log.d("Inside", "processFinished " + questionArrayList);
 
             }
         });
 
-        //Log.d("Main", "onCreate: " + questionList);
+    }
 
+    @Override
+    public void onClick(View v) {
 
+        switch (v.getId()) {
+            case R.id.prev_button:
+                if (currentQuestionIndex >= 1) {
+                    currentQuestionIndex--;
+                }
+                updateQuestion();
+                break;
+            case R.id.next_button:
+                currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+                updateQuestion();
+                break;
+            case R.id.true_button:
+                checkAnswer(true);
+                break;
+            case R.id.false_button:
+                checkAnswer(false);
+                break;
+        }
 
+    }
+
+    private void updateQuestion() {
+        String question = questionList.get(currentQuestionIndex).getAnswer();
+        questionTextview.setText(question);
+        questionCounterTextview.setText(currentQuestionIndex + " / " + questionList.size());
+    }
+
+    private void checkAnswer(boolean userChooseCorrect) {
+        boolean answerIsTrue = questionList.get(currentQuestionIndex).isAnswerTrue();
+        int toastMessageId = 0;
+        if (userChooseCorrect == answerIsTrue) {
+            toastMessageId = R.string.correct_answer;
+        } else {
+            toastMessageId = R.string.wrong_answer;
+        }
+        Toast.makeText(MainActivity.this, toastMessageId, Toast.LENGTH_SHORT).show();
     }
 }
