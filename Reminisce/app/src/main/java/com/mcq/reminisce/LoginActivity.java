213fbbe2db450e1317a7c8a95839fa346f7c7ff0
NewbieMcCoder <Mquinter1988@gmail.com>
2020-10-16 21:58:25 -0400

    Basic UI

--- Reminisce/app/src/main/java/com/mcq/reminisce/LoginActivity.java
+++ Reminisce/app/src/main/java/com/mcq/reminisce/LoginActivity.java
@@ -2,13 +2,29 @@ package com.mcq.reminisce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.email_sign_in_button);
+        createAccountButton = findViewById(R.id.create_acct_button);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, CreateAccountActivity.class));
+            }
+        });
     }
 }
