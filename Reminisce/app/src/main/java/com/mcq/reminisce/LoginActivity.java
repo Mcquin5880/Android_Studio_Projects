f5719c79e7b67fc287d013e125f327808b5abafd
NewbieMcCoder <Mquinter1988@gmail.com>
2020-10-17 17:31:14 -0400

    User authentication

--- Reminisce/app/src/main/java/com/mcq/reminisce/LoginActivity.java
+++ Reminisce/app/src/main/java/com/mcq/reminisce/LoginActivity.java
@@ -6,11 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button loginButton;
     private Button createAccountButton;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
